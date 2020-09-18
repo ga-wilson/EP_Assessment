2816659b5f1c519b39b63488afe5186353260382
ga-wilson <67750116+ga-wilson@users.noreply.github.com>
2020-09-18 12:39:45 -0400

    Add files via upload

---
+++ Enclara Pharmacia Assessment Sol/Enclara Pharmacia Assessment/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Enclara_Pharmacia_Assessment
+{
+    partial class EP_Assessment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EP_Assessment));
+            this.label1 = new System.Windows.Forms.Label();
+            this.input_textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.input_button1 = new System.Windows.Forms.Button();
+            this.search_button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.letter_textBox2 = new System.Windows.Forms.TextBox();
+            this.contain_textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pwn_label5 = new System.Windows.Forms.Label();
+            this.psn_label7 = new System.Windows.Forms.Label();
+            this.unique_textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox19 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Maroon;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(59, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Text Here:";
+            // 
+            // input_textBox1
+            // 
+            this.input_textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input_textBox1.Location = new System.Drawing.Point(349, 45);
+            this.input_textBox1.Multiline = true;
+            this.input_textBox1.Name = "input_textBox1";
+            this.input_textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.input_textBox1.Size = new System.Drawing.Size(541, 236);
+            this.input_textBox1.TabIndex = 0;
+            this.input_textBox1.TextChanged += new System.EventHandler(this.Input_textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Maroon;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(308, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Palindrome Words:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Maroon;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(6, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(367, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Palindrome Sentences:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Maroon;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 37);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Words Used:";
+            // 
+            // input_button1
+            // 
+            this.input_button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input_button1.Location = new System.Drawing.Point(112, 166);
+            this.input_button1.Name = "input_button1";
+            this.input_button1.Size = new System.Drawing.Size(164, 68);
+            this.input_button1.TabIndex = 1;
+            this.input_button1.Text = "Done";
+            this.input_button1.UseVisualStyleBackColor = true;
+            this.input_button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // search_button2
+            // 
+            this.search_button2.Enabled = false;
+            this.search_button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search_button2.Location = new System.Drawing.Point(632, 28);
+            this.search_button2.Name = "search_button2";
+            this.search_button2.Size = new System.Drawing.Size(203, 77);
+            this.search_button2.TabIndex = 1;
+            this.search_button2.Text = "Search";
+            this.search_button2.UseVisualStyleBackColor = true;
+            this.search_button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Maroon;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(31, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(454, 37);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Words Containing the Letter:";
+            // 
+            // letter_textBox2
+            // 
+            this.letter_textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.letter_textBox2.Location = new System.Drawing.Point(522, 47);
+            this.letter_textBox2.Name = "letter_textBox2";
+            this.letter_textBox2.Size = new System.Drawing.Size(82, 44);
+            this.letter_textBox2.TabIndex = 0;
+            this.letter_textBox2.TextChanged += new System.EventHandler(this.Letter_textBox2_TextChanged);
+            // 
+            // contain_textBox3
+            // 
+            this.contain_textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contain_textBox3.Location = new System.Drawing.Point(66, 139);
+            this.contain_textBox3.Multiline = true;
+            this.contain_textBox3.Name = "contain_textBox3";
+            this.contain_textBox3.ReadOnly = true;
+            this.contain_textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.contain_textBox3.Size = new System.Drawing.Size(793, 147);
+            this.contain_textBox3.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.contain_textBox3);
+            this.groupBox1.Controls.Add(this.letter_textBox2);
+            this.groupBox1.Controls.Add(this.search_button2);
+            this.groupBox1.Location = new System.Drawing.Point(50, 804);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(912, 333);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // pwn_label5
+            // 
+            this.pwn_label5.AutoSize = true;
+            this.pwn_label5.BackColor = System.Drawing.Color.Maroon;
+            this.pwn_label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pwn_label5.ForeColor = System.Drawing.Color.White;
+            this.pwn_label5.Location = new System.Drawing.Point(396, 43);
+            this.pwn_label5.Name = "pwn_label5";
+            this.pwn_label5.Size = new System.Drawing.Size(35, 37);
+            this.pwn_label5.TabIndex = 12;
+            this.pwn_label5.Text = "0";
+            this.pwn_label5.Visible = false;
+            // 
+            // psn_label7
+            // 
+            this.psn_label7.AutoSize = true;
+            this.psn_label7.BackColor = System.Drawing.Color.Maroon;
+            this.psn_label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.psn_label7.ForeColor = System.Drawing.Color.White;
+            this.psn_label7.Location = new System.Drawing.Point(396, 102);
+            this.psn_label7.Name = "psn_label7";
+            this.psn_label7.Size = new System.Drawing.Size(35, 37);
+            this.psn_label7.TabIndex = 13;
+            this.psn_label7.Text = "0";
+            this.psn_label7.Visible = false;
+            // 
+            // unique_textBox4
+            // 
+            this.unique_textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unique_textBox4.Location = new System.Drawing.Point(299, 190);
+            this.unique_textBox4.Multiline = true;
+            this.unique_textBox4.Name = "unique_textBox4";
+            this.unique_textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.unique_textBox4.Size = new System.Drawing.Size(541, 236);
+            this.unique_textBox4.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(1013, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 288);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(1013, 289);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(278, 288);
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox19
+            // 
+            this.pictureBox19.BackColor = System.Drawing.Color.Black;
+            this.pictureBox19.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox19.BackgroundImage")));
+            this.pictureBox19.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox19.Location = new System.Drawing.Point(1013, 861);
+            this.pictureBox19.Name = "pictureBox19";
+            this.pictureBox19.Size = new System.Drawing.Size(278, 288);
+            this.pictureBox19.TabIndex = 32;
+            this.pictureBox19.TabStop = false;
+            // 
+            // pictureBox21
+            // 
+            this.pictureBox21.BackColor = System.Drawing.Color.Black;
+            this.pictureBox21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox21.BackgroundImage")));
+            this.pictureBox21.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox21.Location = new System.Drawing.Point(1013, 575);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(278, 288);
+            this.pictureBox21.TabIndex = 34;
+            this.pictureBox21.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.unique_textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.psn_label7);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.pwn_label5);
+            this.groupBox2.Location = new System.Drawing.Point(50, 313);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(912, 459);
+            this.groupBox2.TabIndex = 35;
+            this.groupBox2.TabStop = false;
+            // 
+            // EP_Assessment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(1291, 1149);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.pictureBox21);
+            this.Controls.Add(this.pictureBox19);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.input_button1);
+            this.Controls.Add(this.input_textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "EP_Assessment";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EP Assessment";
+            this.Load += new System.EventHandler(this.EP_Assessment_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input_textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button input_button1;
+        private System.Windows.Forms.Button search_button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox letter_textBox2;
+        private System.Windows.Forms.TextBox contain_textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label pwn_label5;
+        private System.Windows.Forms.Label psn_label7;
+        private System.Windows.Forms.TextBox unique_textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox19;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Enclara Pharmacia Assessment Sol/Enclara Pharmacia Assessment/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enclara_Pharmacia_Assessment
+{
+    public partial class EP_Assessment : Form
+    {
+        public EP_Assessment()
+        {
+            InitializeComponent();
+        }
+
+        List<string> words = new List<string>();
+        List<string> unique_words = new List<string>();
+        List<int> count_unique = new List<int>();
+        List<string> sentences = new List<string>();
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (input_textBox1.Text == "")
+            { MessageBox.Show("Please enter some kind of text."); }
+            else
+            {
+                string input_w = input_textBox1.Text.ToLower();
+                string input_s = input_textBox1.Text.ToLower();
+                int NumPalindromeWords = 0;
+                int NumPalindromeSentences = 0;
+                unique_words.Clear();
+                count_unique.Clear();
+
+                //Compile Word List
+                string word = Remove.Punctuation(input_w);
+                word = Remove.SentenceEnd(word);
+                words = Remove.Space_Store(word, words);
+
+
+                //Compile Word List
+                string sentence = Remove.Punctuation(input_s);
+                sentence = Remove.Space(sentence);
+                sentences = Remove.Sentence_Store(sentence, sentences);
+
+
+                //Count the palindromes
+                for (int i = 0; i < words.Count; i++)
+                {
+                    if (words[i] == Reverse.String(words[i]) && words[i] != "")
+                    {
+                        NumPalindromeWords++;
+                    }
+                }
+                for (int i = 0; i < sentences.Count; i++)
+                {
+                    if (sentences[i] == Reverse.String(sentences[i]) && sentences[i] != "")
+                    {
+                        NumPalindromeSentences++;
+                    }
+                }
+
+
+                //Count the unique words
+                for (int i = 0; i < words.Count; i++)
+                {
+                    if (unique_words.Contains(words[i]))
+                    {
+                        count_unique[unique_words.IndexOf(words[i])] = count_unique[unique_words.IndexOf(words[i])] + 1;
+                    }
+                    else
+                    {
+                        unique_words.Add(words[i]);
+                        count_unique.Add(1);
+                    }
+                }
+
+                //Display
+                pwn_label5.Visible = true;
+                psn_label7.Visible = true;
+                pwn_label5.Text = NumPalindromeWords.ToString();
+                psn_label7.Text = NumPalindromeSentences.ToString();
+                string uniques = "";
+                for (int i = 0; i < unique_words.Count; i++)
+                {
+                    uniques = uniques + unique_words[i] + " [" + count_unique[i].ToString() + " time(s)]" + Environment.NewLine;
+                }
+                unique_textBox4.Text = uniques;
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string search = (letter_textBox2.Text).ToLower();
+            if (search.Length < 2)
+            {
+                string has_letter = "";
+                Boolean exist;
+                for (int i = 0; i < unique_words.Count; i++)
+                {
+                    exist = false;
+                    foreach (char letter in unique_words[i])
+                    {
+                        if (search == letter.ToString())
+                        {
+                            exist = true;
+                        }
+                    }
+                    if (exist)
+                    {
+                        has_letter = has_letter + unique_words[i] + Environment.NewLine;
+                    }
+                }
+                if (has_letter != "")
+                {
+                    contain_textBox3.Text = has_letter;
+                }
+                else
+                {
+                    contain_textBox3.Text = "This character does not appear in your text.";
+                }
+            }
+            else
+            {
+                contain_textBox3.Text = "Please select only 1 character.";
+            }
+        }
+        private void Input_textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            pwn_label5.Text = "";
+            psn_label7.Text = "";
+            unique_textBox4.Text = "";
+            if (input_textBox1.Text == "" || letter_textBox2.Text == "")
+            { search_button2.Enabled = false; }
+            else
+            { search_button2.Enabled = true; }
+        }
+        private void Letter_textBox2_TextChanged(object sender, EventArgs e)
+        {
+            contain_textBox3.Text = "";
+            if (input_textBox1.Text == "" || letter_textBox2.Text == "")
+            { search_button2.Enabled = false; }
+            else
+            { search_button2.Enabled = true; }
+        }
+
+        private void EP_Assessment_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank you so much for looking at my project and for your consideration!");
+        }
+    }
+
+    public static class Remove
+    {
+        public static string Space(string input)
+        {
+            foreach (char space_char in input)
+            {
+                string space = space_char.ToString();
+                if (space == " ")
+                {
+                    input = input.Remove(input.IndexOf(space), 1);
+                }
+            }
+            return input;
+        }
+        public static string SentenceEnd(string input)
+        {
+            foreach (char punct_char in input)
+            {
+                string punct = punct_char.ToString();
+                if (punct == "." || punct == "!" || punct == "?")
+                {
+                    input = input.Remove(input.IndexOf(punct), 1);
+                }
+            }
+            return input;
+        }
+        public static List<string> Space_Store(string input, List<string> store)
+        {
+            store.Clear();
+            foreach (char space_char in input)
+            {
+                string space = space_char.ToString();
+                if (space == " ")
+                {
+                    store.Add(input.Substring(0, input.IndexOf(space)));
+                    //Add a catch for if the +1 is out of range
+                    input = input.Substring(input.IndexOf(space) + 1);
+                }
+            }
+            store.Add(input);
+            return store;
+        }
+        public static List<string> Sentence_Store(string input, List<string> store)
+        {
+            store.Clear();
+            foreach (char punct_char in input)
+            {
+                string punct = punct_char.ToString();
+                if (punct == "." || punct == "!" || punct == "?")
+                {
+                    store.Add(input.Substring(0, input.IndexOf(punct)));
+                    //Add a catch for if the +1 is out of range
+                    input = input.Substring(input.IndexOf(punct) + 1);
+                }
+            }
+            store.Add(input);
+            return store;
+        }
+        public static string Punctuation(string input)
+        {
+            foreach (char punctuation in input)
+            {
+                string punct = punctuation.ToString();
+                if (punct == "," || punct == ";" || punct == ":" || punct == "'" || punct == "\"" || punct == "%" || punct == "$" || punct == "#" || punct == "&" || punct == "(" || punct == ")" || punct == "[" || punct == "]" || punct == "@" || punct == "^" || punct == "*" || punct == "_" || punct == "-" || punct == "+" || punct == "=" || punct == "/" || punct == "<" || punct == ">" || punct == "|" || punct == "\\" || punct == "`" || punct == "~")
+
+                {
+                    input = input.Remove(input.IndexOf(punct), 1);
+                }
+            }
+            return input;
+        }
+    }
+
+    public static class Reverse
+    {
+        public static string String(string initial)
+        {
+            string final = "";
+            foreach (char letter in initial)
+            {
+                final = letter.ToString() + final;
+            }
+            return final;
+        }
+    }
+}
+
+//        //Give the number of palindrome words
+//        //Done :)
+//        //Give the number of palindrome sentences
+//        //Done :)
+//        //List the unique words of a paragraph with the count of the word instance
+//        //Done :)
+//        //Let the user also input a letter at some point and list all words containing that letter
